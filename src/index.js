70072eb88e8645ac35948fd3f3e24a8f9e176b62
Justin Livi <justin@brinkbit.com>
2017-01-09 13:52:36 -0500

    Use papertrail instead of slack for info

--- src/index.js
+++ src/index.js
@@ -4,35 +4,39 @@ const winston = require( 'winston' );
 const morgan = require( 'morgan' );
 const stackTrace = require( 'stack-trace' );
 const Slack = require( 'winston-slack-hook' );
+const Papertrail = require( 'winston-papertrail' ).Papertrail;
 
 module.exports = config => {
     const c = config || {};
     c.slack = c.slack || {};
-    let production = [];
+    c.paperTrail = c.paperTrail || {};
+
+    const production = [];
     if ( c.slack.hookUrl || process.env.SLACK_HOOK ) {
-        production = production.concat([
-            new Slack({
-                name: 'standardHookSlack',
-                level: 'info',
-                team: c.slack.team || process.env.SLACK_TEAM,
-                hookUrl: c.slack.hookUrl || process.env.SLACK_HOOK,
-                channel: c.slack.channel || process.env.SLACK_CHANNEL,
-                username: c.slack.username || process.env.SLACK_USERNAME,
-                appendMeta: false,
-            }),
-            new Slack({
-                name: 'critHookSlack',
-                level: 'crit',
-                team: c.slack.team || process.env.SLACK_TEAM,
-                hookUrl: c.slack.hookUrl || process.env.SLACK_HOOK,
-                channel: c.slack.critChannel || process.env.SLACK_CRIT_CHANNEL,
-                username: c.slack.username || process.env.SLACK_USERNAME,
-                prependLevel: false,
-                formatter: options => {
-                    return `${options.level === 'crit' ? '@all CRITICAL ERROR: ' : ''} ${options.message}`;
-                },
-            }),
-        ]);
+        production.push( new Slack({
+            name: 'critHookSlack',
+            level: 'crit',
+            team: c.slack.team || process.env.SLACK_TEAM,
+            hookUrl: c.slack.hookUrl || process.env.SLACK_HOOK,
+            channel: c.slack.critChannel || process.env.SLACK_CHANNEL,
+            username: c.slack.username || process.env.SLACK_USERNAME,
+            prependLevel: false,
+            formatter: options => {
+                return `${options.level === 'crit' ? '@all CRITICAL ERROR: ' : ''}${options.message}`;
+            },
+        }));
+    }
+    if ( c.paperTrail.host || process.env.PAPERTRAIL_HOST ) {
+        const papertrail = new Papertrail({
+            level: 'info',
+            levels: winston.config.syslog.levels,
+            host: c.paperTrail.host || process.env.PAPERTRAIL_HOST,
+            port: c.paperTrail.port || process.env.PAPERTRAIL_PORT,
+            program: c.paperTrail.program || process.env.DOCKERCLOUD_SERVICE_HOSTNAME,
+            hostname: c.paperTrail.hostname || process.env.DOCKERCLOUD_NODE_HOSTNAME,
+            colorize: true,
+        });
+        production.push( papertrail );
     }
     production.push( new winston.transports.File({
         silent: false,
@@ -40,8 +44,8 @@ module.exports = config => {
         filename: c.logFile || process.env.LOG_FILE || 'logs.log',
         handleExceptions: true,
         json: true,
-        maxsize: 5242880,
-        maxFiles: 5,
+        maxsize: c.logSize || process.env.LOG_SIZE || 5242880,
+        maxFiles: c.logCount || process.env.LOG_COUNT || 5,
         colorize: false,
     }));
     const transports = {
@@ -66,7 +70,7 @@ module.exports = config => {
         })],
     };
     const transport = transports[c.transport || process.env.NODE_ENV] ? c.transport || process.env.NODE_ENV : 'production';
-    const morganFormat = transport === 'development' || transport === 'debug' ? 'dev' : 'combined';
+    const morganFormat = transport === 'development' || transport === 'debug' ? 'dev' : 'short';
     const logger = winston.loggers.get( c.__filename, {
         transports: transports[transport],
     });
@@ -83,30 +87,25 @@ module.exports = config => {
             return `"${msg}"   at ${callsite.getFunctionName() || '<anonymous>'} (${callsite.getFileName()}:${callsite.getLineNumber()}:${callsite.getColumnNumber()})`;
         });
     }
-    if ( transport === 'production' &&
-        process.env.DOCKERCLOUD_CONTAINER_HOSTNAME &&
-        process.env.DOCKERCLOUD_STACK_NAME &&
-        logger.filters < 1 ) {
-        logger.filters.push(( level, msg ) => {
-            return `${process.env.DOCKERCLOUD_STACK_NAME}:${process.env.DOCKERCLOUD_CONTAINER_HOSTNAME} | ${msg}`;
-        });
-    }
 
     if ( transport === 'development' && logger.rewriters < 1 ) {
         logger.rewriters.push(() => {
             return {};
         });
     }
-    else if ( transport !== 'debug' && transport !== 'test' && logger.rewriters < 1 ) {
+    else if ( transport === 'production' && logger.rewriters < 1 ) {
         logger.rewriters.push(( level, msg, meta ) => {
             if ( meta.isMiddleware ) delete meta.isMiddleware;
             else meta.filename = c.__filename;
+            if ( process.env.DOCKERCLOUD_CONTAINER_HOSTNAME ) meta.containerName = process.env.DOCKERCLOUD_CONTAINER_HOSTNAME;
+            if ( process.env.DOCKERCLOUD_SERVICE_HOSTNAME ) meta.serviceName = process.env.DOCKERCLOUD_SERVICE_HOSTNAME;
+            if ( process.env.DOCKERCLOUD_STACK_NAME ) meta.stackName = process.env.DOCKERCLOUD_STACK_NAME;
             return meta;
         });
     }
 
     if ( transport !== 'test' ) {
-        logger.middleware = morgan( morganFormat, { 'stream': {
+        logger.middleware = morgan( morganFormat, { stream: {
             write: ( message ) => {
                 logger.info( message, { isMiddleware: true });
             },
--- test/index.js
+++ test/index.js
@@ -15,144 +15,154 @@ const expect = chai.expect;
 chai.use( chaiaspromised );
 chai.use( sinonchai );
 
-describe( 'configuration', function() {
-    it( 'should not crash when missing slack property', function() {
-        process.env.SLACK_HOOK = 'invalid';
-        require( '../src' )({ __filename: Math.random(), transport: 'debug' });
-    });
-});
+describe( 'brinkbit-logger', function() {
+    this.timeout( 10000 );
 
-describe( '/debug', function() {
-    it( 'should console log verbose message on debug', function( done ) {
-        const logger = require( '../src' )({ __filename: Math.random(), transport: 'debug' });
-        logger.on( 'logging', ( transport, level, msg, meta ) => {
-            expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
-            expect( level ).to.equal( 'debug' );
-            expect( msg ).to.match( /\"message\stext\"\s\s\sat\s\<anonymous\>\s\(/gi );
-            expect( meta ).to.deep.equal({ data: 'some metadata' });
-            done();
+    describe( 'configuration', function() {
+        it( 'should not crash when missing slack property', function() {
+            process.env.SLACK_HOOK = 'invalid';
+            require( '../src' )({ __filename: Math.random(), transport: 'debug' });
         });
-        logger.debug( 'message text', { data: 'some metadata' });
     });
-});
 
-describe( '/development', function() {
-    it( 'should console log just the message on debug', function( done ) {
-        const logger = require( '../src' )({ __filename: Math.random(), transport: 'development' });
-        logger.on( 'logging', ( transport, level, msg, meta ) => {
-            expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
-            expect( level ).to.equal( 'info' );
-            expect( msg ).to.equal( 'message text' );
-            expect( meta ).to.deep.equal({});
-            done();
-        });
-        logger.info( 'message text', { data: 'some metadata' });
-    });
-});
-
-describe( '/production', function() {
-    process.env.DOCKERCLOUD_CONTAINER_HOSTNAME = 'testHost';
-    process.env.DOCKERCLOUD_STACK_NAME = 'test-stack-name';
-
-    it( 'should log crits to all three transports', function( done ) {
-        const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
-        let count = 0;
-        logger.on( 'logging', ( transport, level, msg ) => {
-            expect( msg ).to.equal( 'test-stack-name:testHost | testing critical messages' );
-            count++;
-            if ( count > 2 ) {
+    describe( '/debug', function() {
+        it( 'should console log verbose message on debug', function( done ) {
+            const logger = require( '../src' )({ __filename: Math.random(), transport: 'debug' });
+            logger.on( 'logging', ( transport, level, msg, meta ) => {
+                expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
+                expect( level ).to.equal( 'debug' );
+                expect( msg ).to.match( /\"message\stext\"\s\s\sat\s\<anonymous\>\s\(/gi );
+                expect( meta ).to.deep.equal({ data: 'some metadata' });
                 done();
-            }
-        });
-        logger.crit( 'testing critical messages' );
-    });
-
-    it( 'should not log debugs to standardSlack', function() {
-        const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
-        logger.debug( 'testing debug messages' );
-    });
-
-    it( 'should log info to standardSlack and file', function( done ) {
-        const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
-        let count = 0;
-        logger.on( 'logging', ( transport, level, msg ) => {
-            expect( transport ).to.have.property( 'name' ).and.match( /standardHookSlack|file/g );
-            expect( msg ).to.equal( 'test-stack-name:testHost | testing info messages' );
-            count++;
-            if ( count > 1 ) {
-                done();
-            }
-        });
-        logger.info( 'testing info messages' );
-    });
-});
-
-describe( '/test', function() {
-    it( 'should not crit anything', function( done ) {
-        const logger = require( '../src' )({ __filename: Math.random(), transport: 'test' });
-        const spy = sinon.spy();
-        logger.on( 'logging', spy );
-        logger.crit( 'should crit verbose message and standard meta' );
-        setTimeout(() => {
-            expect( spy ).to.not.have.been.called;
-            done();
-        }, 10 );
-    });
-
-    it( 'should console log message on emergency', function( done ) {
-        const logger = require( '../src' )({ __filename: Math.random(), transport: 'test' });
-        logger.on( 'logging', ( transport, level, msg, meta ) => {
-            expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
-            expect( level ).to.equal( 'emerg' );
-            expect( msg ).to.equal( 'message text' );
-            expect( meta ).to.deep.equal({ data: 'some metadata' });
-            done();
-        });
-        logger.emerg( 'message text', { data: 'some metadata' });
-    });
-});
-
-describe( 'middleware', function() {
-    it( 'should console log requests', function( done ) {
-        const logger = require( '../src' )({ __filename: Math.random(), transport: 'debug' });
-        const middleware = logger.middleware;
-        logger.on( 'logging', ( transport, level, msg ) => {
-            expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
-            expect( level ).to.equal( 'info' );
-            expect( msg.substr( 0, 37 )).to.match( /GET\s\/\s/g );
-            done();
-        });
-        request( http.createServer(( req, res ) => {
-            middleware( req, res, err => {
-                if ( err ) {
-                    res.statusCode = 500;
-                    res.end( err.message );
-                }
-
-                res.setHeader( 'X-Sent', 'true' );
-                res.end(( req.connection && req.connection.remoteAddress ) || '-' );
             });
-        }))
-        .get( '/' )
-        .expect( 200, () => {});
-    });
-});
-
-describe( 'environment variables', function() {
-    process.env.NODE_ENV = 'test';
-
-    it( 'should not info anything', function() {
-        const logger = require( '../src' )({ __filename: Math.random() });
-        logger.info( 'should not info anything' );
+            logger.debug( 'message text', { data: 'some metadata' });
+        });
     });
 
-    it( 'should not error anything', function() {
-        const logger = require( '../src' )({ __filename: Math.random() });
-        logger.info( 'should not error anything' );
+    describe( '/development', function() {
+        it( 'should console log just the message on debug', function( done ) {
+            const logger = require( '../src' )({ __filename: Math.random(), transport: 'development' });
+            logger.on( 'logging', ( transport, level, msg, meta ) => {
+                expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
+                expect( level ).to.equal( 'info' );
+                expect( msg ).to.equal( 'message text' );
+                expect( meta ).to.deep.equal({});
+                done();
+            });
+            logger.info( 'message text', { data: 'some metadata' });
+        });
     });
 
-    it( 'should crit verbose message and standard meta', function() {
-        const logger = require( '../src' )({ __filename: Math.random() });
-        logger.crit( 'should crit verbose message and standard meta' );
+    describe( '/production', function() {
+        process.env.DOCKERCLOUD_SERVICE_HOSTNAME = 'testService';
+        process.env.DOCKERCLOUD_CONTAINER_HOSTNAME = 'testHost';
+        process.env.DOCKERCLOUD_NODE_HOSTNAME = 'testNodeHost';
+        process.env.DOCKERCLOUD_STACK_NAME = 'test-stack-name';
+
+        it( 'should log crits to all three transports', function( done ) {
+            const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
+            logger.transports.Papertrail.on( 'connect', () => {
+                let count = 0;
+                logger.on( 'logging', ( transport, level, msg ) => {
+                    expect( msg ).to.equal( 'testing critical messages' );
+                    count++;
+                    if ( count > 2 ) {
+                        done();
+                    }
+                });
+                logger.crit( 'testing critical messages' );
+            });
+        });
+
+        it( 'should not log debugs to standardSlack', function() {
+            const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
+            logger.debug( 'testing debug messages' );
+        });
+
+        it( 'should log info to Papertrail and file', function( done ) {
+            const logger = require( '../src' )( R.merge({ __filename: Math.random(), transport: 'production' }, config ));
+            logger.transports.Papertrail.on( 'connect', () => {
+                let count = 0;
+                logger.on( 'logging', ( transport, level, msg ) => {
+                    expect( transport ).to.have.property( 'name' ).and.match( /Papertrail|file/g );
+                    expect( msg ).to.equal( 'testing info messages' );
+                    count++;
+                    if ( count > 1 ) {
+                        done();
+                    }
+                });
+                logger.info( 'testing info messages' );
+            });
+        });
+    });
+
+    describe( '/test', function() {
+        it( 'should not crit anything', function( done ) {
+            const logger = require( '../src' )({ __filename: Math.random(), transport: 'test' });
+            const spy = sinon.spy();
+            logger.on( 'logging', spy );
+            logger.crit( 'should crit verbose message and standard meta' );
+            setTimeout(() => {
+                expect( spy ).to.not.have.been.called;
+                done();
+            }, 10 );
+        });
+
+        it( 'should console log message on emergency', function( done ) {
+            const logger = require( '../src' )({ __filename: Math.random(), transport: 'test' });
+            logger.on( 'logging', ( transport, level, msg, meta ) => {
+                expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
+                expect( level ).to.equal( 'emerg' );
+                expect( msg ).to.equal( 'message text' );
+                expect( meta ).to.deep.equal({ data: 'some metadata' });
+                done();
+            });
+            logger.emerg( 'message text', { data: 'some metadata' });
+        });
+    });
+
+    describe( 'middleware', function() {
+        it( 'should console log requests', function( done ) {
+            const logger = require( '../src' )({ __filename: Math.random(), transport: 'debug' });
+            const middleware = logger.middleware;
+            logger.on( 'logging', ( transport, level, msg ) => {
+                expect( transport ).to.have.property( 'name' ).and.equal( 'console' );
+                expect( level ).to.equal( 'info' );
+                expect( msg.substr( 0, 37 )).to.match( /GET\s\/\s/g );
+                done();
+            });
+            request( http.createServer(( req, res ) => {
+                middleware( req, res, err => {
+                    if ( err ) {
+                        res.statusCode = 500;
+                        res.end( err.message );
+                    }
+
+                    res.setHeader( 'X-Sent', 'true' );
+                    res.end(( req.connection && req.connection.remoteAddress ) || '-' );
+                });
+            }))
+            .get( '/' )
+            .expect( 200, () => {});
+        });
+    });
+
+    describe( 'environment variables', function() {
+        process.env.NODE_ENV = 'test';
+
+        it( 'should not info anything', function() {
+            const logger = require( '../src' )({ __filename: Math.random() });
+            logger.info( 'should not info anything' );
+        });
+
+        it( 'should not error anything', function() {
+            const logger = require( '../src' )({ __filename: Math.random() });
+            logger.info( 'should not error anything' );
+        });
+
+        it( 'should crit verbose message and standard meta', function() {
+            const logger = require( '../src' )({ __filename: Math.random() });
+            logger.crit( 'should crit verbose message and standard meta' );
+        });
     });
 });
